675820ec1ffce56bda6b3fb1ad5884890c4ca983
tanui-ivy <ivy.tanui@student.moringaschool.com>
2025-03-03 09:48:07 +0300

    Fetching plant data

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage(){
+  //to store fetched plants
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((res) => res.json())
+    .then((data) => setPlants(data));//store the plants in state
+    }, []);
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
