3ef825b84b00704ab4cb5e75ba264670f8bda92d
Ivy Tanui <ivy.tanui@strathmore.edu>
2025-03-03 11:15:41 +0300

    I can change a plant to Sold Out

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+
+
 function App() {
   return (
     <div className="app">
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 
 function PlantCard({name,image,price}) {
+  const [inStock, setInStock] = useState(true);//the plant by default will be in stock
+
+  function handleStockToggle(){
+    setInStock(!inStock);// a toggle between in stock and sold out
+  }
+
   return (
     <li className="card" data-testid="plant-item">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: ${price}</p>
-      <button className="primary">In Stock</button>
+      <button
+      className= {inStock ? "primary" : ""}
+      onClick={handleStockToggle}
+       >
+      {inStock ? "In Stock" : "Sold Out"}
+      </button>
     </li>
   );
 }
